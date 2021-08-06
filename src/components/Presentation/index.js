a3604a83fa8bc1e1ed33739d1e24807b6efd9107
Osee-Ralantoarison <o.ralanto@gmail.com>
2021-08-06 14:05:13 +0200

    Adding Weather project

--- src/components/Presentation/index.js
+++ src/components/Presentation/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-// import circleScatter from "../../img/circle-scatter-haikei.png";
-
 import "./styles.css";
 
 const Presentation = () => {
--- src/components/Projets/index.js
+++ src/components/Projets/index.js
@@ -12,6 +12,7 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 import Oboardgame from "../../img/Logo-oboardgame.png";
 import Converter from "../../img/Converter-spe-react.png";
 import Chatroom from "../../img/Chatroom-spe-react.png";
+import Weather from "../../img/WeatherApp.png";
 import github from "../../img/github.png";
 
 import "./style.css";
@@ -31,7 +32,10 @@ const Projets = () => {
     <div className="Projets" id="projets">
       <h2 className="Projets__title">Projets</h2>
       <div className="Projets__cards">
-        <Card style={{ backgroundColor: "#41464B" }} className={classes.root + " Projets__card"}>
+        <Card
+          style={{ backgroundColor: "#41464B" }}
+          className={classes.root + " Projets__card"}
+        >
           <CardActionArea>
             <CardMedia
               className={classes.media}
@@ -45,6 +49,9 @@ const Projets = () => {
               <Typography variant="h6" component="p">
                 Projet de formation: Site de location de jeux de sociétés entre
                 particulier.
+              </Typography>
+              <Typography variant="h6" component="p">
+                React-Redux, Node, Express, AWS, postgreSQL.
               </Typography>
             </CardContent>
           </CardActionArea>
@@ -62,19 +69,25 @@ const Projets = () => {
             </Button>
           </CardActions>
         </Card>
-        <Card style={{ backgroundColor: "#41464B" }} className={classes.root + " Projets__card"}>
+        <Card
+          style={{ backgroundColor: "#41464B" }}
+          className={classes.root + " Projets__card"}
+        >
           <CardActionArea>
             <CardMedia
               className={classes.media}
               image={Converter}
-              title="oboardgame logo"
+              title="Converter app image"
             />
             <CardContent style={{ color: "#CECBC5" }} variant="h6">
               <Typography gutterBottom variant="h5" component="h2">
-              Converter
+                Converter
               </Typography>
               <Typography variant="h6" component="p">
-              Projet React: Convertisseur de devise
+                Projet React: Convertisseur de devise.
+              </Typography>
+              <Typography variant="h6" component="p">
+                React, SCSS.
               </Typography>
             </CardContent>
           </CardActionArea>
@@ -92,19 +105,61 @@ const Projets = () => {
             </Button>
           </CardActions>
         </Card>
-        <Card style={{ backgroundColor: "#41464B" }} className={classes.root + " Projets__card"}>
+        <Card
+          style={{ backgroundColor: "#41464B" }}
+          className={classes.root + " Projets__card"}
+        >
           <CardActionArea>
             <CardMedia
               className={classes.media}
               image={Chatroom}
-              title="oboardgame logo"
+              title="Chatroom app image"
             />
             <CardContent style={{ color: "#CECBC5" }} variant="h6">
               <Typography gutterBottom variant="h5" component="h2">
-              Chatroom
+                Chatroom
               </Typography>
               <Typography variant="h6" component="p">
-              Projet React: Simulation de Chatroom
+                Projet React: Simulation de Chatroom.
+              </Typography>
+              <Typography variant="h6" component="p">
+                React-redux, SCSS.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              <GitHubIcon style={{ color: "black", margin: "1rem" }} />
+              <a
+                className="Projets__card__link"
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/oralanto/Chatroom-v1-React"
+              >
+                Voir le projet
+              </a>
+            </Button>
+          </CardActions>
+        </Card>
+        <Card
+          style={{ backgroundColor: "#41464B" }}
+          className={classes.root + " Projets__card"}
+        >
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image={Weather}
+              title="weather app image"
+            />
+            <CardContent style={{ color: "#CECBC5" }} variant="h6">
+              <Typography gutterBottom variant="h5" component="h2">
+                Weather app
+              </Typography>
+              <Typography variant="h6" component="p">
+                Projet React: Weather app.
+              </Typography>
+              <Typography variant="h6" component="p">
+                React, Material UI, API: openWeather.
               </Typography>
             </CardContent>
           </CardActionArea>
@@ -123,14 +178,18 @@ const Projets = () => {
           </CardActions>
         </Card>
       </div>
-      <Button  color="inherit">
+      <Button color="inherit">
         <a
           className="Projets__github__link"
           target="_blank"
           rel="noreferrer"
           href="https://www.github.com/oralanto/"
         >
-          <img className="Projets__github__logo" src={github} alt="Icon Github" />
+          <img
+            className="Projets__github__logo"
+            src={github}
+            alt="Icon Github"
+          />
           <p className="Projets__github__text">Mon Github</p>
         </a>
       </Button>
